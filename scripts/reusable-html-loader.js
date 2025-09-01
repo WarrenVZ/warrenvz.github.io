0ca8068bce642a262015c99e18a746a8c547f783
WarrenVZ <warrenvz@protonmail.com>
2025-09-01 12:47:18 +0200

    Add files via upload

--- scripts/reusable-html-loader.js
+++ scripts/reusable-html-loader.js
@@ -6,14 +6,13 @@ async function loadHTML(url, targetSelector) {
 
     if (targetElement) {
       targetElement.innerHTML = htmlContent;
-      // If your navigation has interactive elements like the hamburger button,
-      // you might need to call the function to enable them here,
-      // especially if it wasn't already called after loading nav.html previously.
 
 
-      if (url === '/nav2.html') { // Adds the hamburger functionality to nav2.html!!!
+
+      if (url === '/nav/nav2.html') { // Adds the hamburger functionality to nav2.html!!!
         addHamburgerFunctionality();
       }
+
 
 
 
@@ -25,6 +24,9 @@ async function loadHTML(url, targetSelector) {
     console.error(`Failed to load content from ${url}:`, error);
   }
 }
+
+
+
 
 function addHamburgerFunctionality() {
   const hamburger = document.querySelector('.hamburger');
@@ -38,27 +40,20 @@ function addHamburgerFunctionality() {
   }
 }
 
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
 
-  // If you're using GitHub Pages, include your repo
-  // name, such as /website/nav.html. Doing this will
-  // make your offline webpage not be able to fetch
-  // the HTML, which is fine. You can edit out your
-  // repo name while you're editing offline, but
-  // remember to re-enable it!!! Also, remember the
-  // if statement above that adds the hamburger
-  // functionality! Add your repo name there too!
-  // Alternatively, you can duplicate the code into
-  // two different versions, and comment out depending
-  // if you're editing offline, or uploading to live.
 
 
-  // REPLACE "/website/" (YOUR REPO NAME) WITH "../../" WHEN EDITING OFFLINE
-  
-  loadHTML('/nav2.html', 'header');
+  loadHTML('/nav/nav2.html', 'header');
   loadHTML('/footer.html', 'footer');
 
-
+  // loadHTML('../../nav.html', 'header');
+  // loadHTML('../../footer.html', 'footer');
 
   // You can add other loadHTML calls here for your footer, sidebar, etc.
 });
