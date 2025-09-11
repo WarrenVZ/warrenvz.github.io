7210cd0fad515f9ac966e36a925889f87f31b4e0
WarrenVZ <warrenvz@protonmail.com>
2025-09-11 15:35:09 +0200

    Add files via upload

---
+++ scripts/reusable-html-loader.js
@@ -0,0 +1,53 @@
+async function loadHTML(url, targetSelector) {
+  try {
+    const response = await fetch(url);
+    const htmlContent = await response.text();
+    const targetElement = document.querySelector(targetSelector);
+
+    if (targetElement) {
+      targetElement.innerHTML = htmlContent;
+
+
+
+      // if (url === '/nav/nav2.html') { // Adds the hamburger functionality to nav2.html!!!
+      //   addHamburgerFunctionality();
+      // }
+
+
+
+
+
+    } else {
+      console.error(`No element found with the selector: ${targetSelector} to inject content from ${url}.`);
+    }
+  } catch (error) {
+    console.error(`Failed to load content from ${url}:`, error);
+  }
+}
+
+
+
+
+// function addHamburgerFunctionality() {
+//   const hamburger = document.querySelector('.hamburger');
+//   const navLinks = document.querySelector('.nav-links');
+
+//   if (hamburger && navLinks) {
+//     hamburger.addEventListener('click', () => {
+//       navLinks.classList.toggle('active');
+//       hamburger.classList.toggle('active');
+//     });
+//   }
+// }
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  loadHTML('/reusable HTML components/nav.html', 'header');
+  loadHTML('/reusable HTML components/footer.html', 'footer');
+
+});
